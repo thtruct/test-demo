c39b8e10d7e6b57c793bd8ef72095d6bf566d7e0
thtruct <95370969+thtruct@users.noreply.github.com>
2023-04-08 19:00:54 +0700

    Add files via upload

---
+++ server/server/Sever.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace server
+{
+    partial class Sever
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnGui = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(80, 39);
+            this.txtChat.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(947, 359);
+            this.txtChat.TabIndex = 11;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(1088, 79);
+            this.btnStop.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(109, 52);
+            this.btnStop.TabIndex = 13;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnGui
+            // 
+            this.btnGui.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGui.Location = new System.Drawing.Point(892, 448);
+            this.btnGui.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnGui.Name = "btnGui";
+            this.btnGui.Size = new System.Drawing.Size(135, 60);
+            this.btnGui.TabIndex = 12;
+            this.btnGui.Text = "Send";
+            this.btnGui.UseVisualStyleBackColor = true;
+            this.btnGui.Click += new System.EventHandler(this.btnGui_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(1088, 212);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(109, 52);
+            this.btnStart.TabIndex = 10;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(80, 448);
+            this.txtMessage.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(787, 60);
+            this.txtMessage.TabIndex = 14;
+            this.txtMessage.TextChanged += new System.EventHandler(this.txtMessage_TextChanged);
+            // 
+            // Sever
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1423, 648);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnGui);
+            this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Sever";
+            this.Text = "Sever";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnGui;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtMessage;
+    }
+}
+
---
+++ server/server/Sever.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace server
+{
+    public partial class Sever : Form
+    {
+
+        public Sever()
+        {
+            InitializeComponent();
+        }
+        private TcpListener server;
+        private readonly List<Clients> clients = new List<Clients>();
+        private Thread thread;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            server = new TcpListener(IPAddress.Any, 9050);
+            server.Start();
+
+            thread = new Thread(() =>
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    Clients clientHandler = new Clients(client, txtChat);
+                    clients.Add(clientHandler);
+                }
+            });
+
+            thread.Start();
+
+            txtChat.AppendText("Server started!\n");
+
+        }
+
+        private void btnGui_Click(object sender, EventArgs e)
+        {
+
+            string message = txtMessage.Text;
+
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            foreach (Clients clientHandler in clients)
+            {
+                clientHandler.SendMessage(messageBytes);
+            }
+
+            txtChat.AppendText("Server: " + message + "\n" + Environment.NewLine);
+            txtMessage.Clear();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+
+            this.Hide();
+
+        }
+        class Clients
+        {
+            private readonly TcpClient tcpClient;
+            private readonly NetworkStream stream;
+            private readonly Thread client_thread;
+            private readonly TextBox chat_box;
+
+            public Clients(TcpClient client, TextBox chatTextBox)
+            {
+                tcpClient = client;
+                stream = client.GetStream();
+                chat_box = chatTextBox;
+
+                client_thread = new Thread(() =>
+                {
+                    byte[] buffer = new byte[1024];
+                    while (true)
+                    {
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        if (bytesRead > 0)
+                        {
+
+                            string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                            chat_box.Invoke(new Action(() =>
+                            {
+                                chat_box.AppendText("Client: " + message + "\n" + Environment.NewLine);
+                            }));
+                        }
+                    }
+                });
+
+                client_thread.Start();
+            }
+            public void SendMessage(byte[] messageBytes)
+            {
+                stream.Write(messageBytes, 0, messageBytes.Length);
+            }
+        }
+
+        private void txtMessage_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
